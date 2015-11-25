fc717a0087e9c7802c10e05e5814c5bf017033e9
Eduardo Vera Sousa <eduardoverasousa@outlook.com>
2015-11-25 17:59:49 -0200

    Data structure

--- dcel.h
+++ dcel.h
@@ -1,16 +1,43 @@
-#ifndef DCEL
-#define DCEL
-
 #include <iostream>
 #include <vector>
+#include <string>
+
+using namespace std;
 
 namespace DCEL {
+
+    struct Vertex;
+    struct Edge;
+
 
     struct Vertex {
         double x;
         double y;
 
         int index;
+
+        Edge *edgeIn;
+        Edge *edgeOut;
+
+        Vertex(double x, double y) {
+            this->x = x;
+            this->y = y;
+        }
+
+        inline bool operator < (const Vertex v) const {
+            if (y > v.y || (y == v.y && x > v.x)) {
+                return true;
+            }
+            return false;
+        }
+
+        inline bool operator < (const Vertex *v) const {
+            if (y > v->y || (y == v->y && x > v->x)) {
+                return true;
+            }
+            return false;
+        }
+
     };
 
     std::vector<Vertex> vertexes;
@@ -19,22 +46,42 @@ namespace DCEL {
         Vertex *src;
         Vertex *tgt;
 
-        Edge() {
+        Edge() {}
 
+        Edge(Vertex *src, Vertex *tgt) {
+            this->src = src;
+            this->tgt = tgt;
         }
 
+        string printEdge() {
+            string content;
+            return content;
+        }
     };
 
     std::vector<Edge> edges;
+    std::vector<Vertex*> queue;
+
 
     void insert(Vertex &p) {
         p.index = vertexes.size();
         vertexes.push_back(p);
-        Edge e()
     }
 
+    void generateDS() {
+        edges.push_back(Edge(&vertexes[vertexes.size() - 1], &vertexes[0]));
+        for (size_t i = 1; i < vertexes.size(); i++) {
+            edges.push_back(Edge(&vertexes[i - 1], &vertexes[i]));
+        }
+
+        for (size_t i = 0; i < vertexes.size() - 1; i++) {
+            vertexes[i].edgeIn = &edges[i];
+            vertexes[i].edgeOut = &edges[i + 1];
+            queue.push_back(&vertexes[i]);
+        }
+        vertexes[vertexes.size() - 1].edgeIn = &edges[edges.size() - 1];
+        vertexes[vertexes.size() - 1].edgeOut = &edges[0];
+        queue.push_back(&vertexes[vertexes.size() - 1]);
+    }
 
 }
-
-#endif // DCEL
-
--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <dcel.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+using namespace DCEL;
 
 int main() {
+    Vertex v1(0, 0);
+    insert(v1);
 
+    Vertex v2(10, 10);
+    insert(v2);
 
+    Vertex v3(15, 16);
+    insert(v3);
 
+    generateDS();
+
+    std::sort(queue.begin(), queue.end());
+
+    for (Vertex *v : queue) {
+        cout << v->index << " left: " << v->edgeIn->src->index << " right: " << v->edgeOut->tgt->index << endl;
+    }
 }
 
